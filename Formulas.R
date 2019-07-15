97e111fb0ce831ae053982d6dcefa3b333858b29
Dmitri <dleybman@gmail.com>
2019-07-14 21:16:58 -0400

    Adding cleaned up version of objects in script

--- Formulas.R
+++ Formulas.R
@@ -6,10 +6,75 @@ dtTwoColumn <-data.table(a= c("1","2","3","4"),
                          d = c(TRUE, FALSE, TRUE, TRUE))
 
 
+dtThreeColumn <-data.table(a= c("1","2","3","4"),
+                         b= c("1", "2", "3", "4"), 
+                         c= c("cat", "stars", "songs", "movies"))
 
 
+
+identicalColumns <- Reduce(intersect, list(names(dtonecolumn),names(dtTwoColumn)))
+
+identicalColumnsBetweenTwoDT <- Reduce(intersect, list(names(dtTwoColumn),names(dtThreeColumn)))
+
+
+
+#####columnsOfInterest <- c("a", "b", "c")
+
+dtTwoColumn[, identicalColumnsInMoreComplexExample, with=FALSE]
+
+identicalColumnsInMoreComplexExample <- as.vector(Reduce(intersect, list(names(dtTwoColumn),names(dtThreeColumn))))
+
+classOfColumnsInMoreComplex <- sapply(dtTwoColumn,class)[identicalColumns]
+
+
+classOfColumnsInMoreComplex <- sapply(dtTwoColumn,class)[columnsOfInterest]
+
+
+classOfColumnsInMoreComplex <- sapply(dtTwoColumn,class)[columnsOfInterest]
+
+
+  dtTwoColumn[, (columnsOfInterest) := 1]
+
+classOfColumnsinFirstTable <- class(dtThreeColumn[[identicalColumns]])
+week <- paste0( "as.", classof, sep="")
+
+
+week <- paste0( "as.", classOfColumnsInMoreComplex, sep="")
+
+
+dtTwoColumn[, (identicalColumnsInMoreComplexExample) := lapply(.SD, classesOfColumns), .SDcols = identicalColumnsInMoreComplexExample]
+
+classesOfColumns <- c("as.numeric", "as.character", "as.numeric")
+
+for i in 1:length(identicalColumnsInMoreComplexExample) {
+  
+  dtTwoColumn[, identicalColumnsInMoreComplexExample[j] := lapply(.SD, classofColumns[j]), .SDcols = identicalColumnsInMoreComplexExample]
+  
+  
+}
+
+
+
+
+dtnew <- dt[, lapply(.SD, as.character), by=ID]
+
+
+dtTwoColumn[, (identicalColumns) := lapply(.SD, as.numeric), .SDcols=identicalColumns]
+
+
+R> d <- c("Mon","Tues","Wednes","Thurs","Fri","Satur","Sun")
+week <- paste0( "as.", classOfColumnsinFirstTable, sep="")
+
+
+
+
+
+partOfFunction()
 
 f <- dtTwoColumn[, a]:= paste0("as.", class(identicalColumns))
+
+
+
 
 
 
